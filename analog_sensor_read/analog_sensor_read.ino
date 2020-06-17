1befbc38373a0042cc2524da8444af961da004e1
MOULESH <moulesh23@gmail.com>
2020-06-17 15:07:40 +0530

    Add files via upload

---
+++ analog_sensor_read/analog_sensor_read.ino
@@ -0,0 +1,40 @@
+// 2 grids=910
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "c8717ecaf7404205bbdaff63c861e5cd";
+
+
+char ssid[] = "jahnavi";
+char pass[] = "12345678";
+
+WidgetLCD lcd(V1);
+int a;
+void setup()
+{
+  
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  Blynk.begin(auth, ssid, pass);
+
+  lcd.clear(); 
+  lcd.print(0, 0, "Water Level"); 
+
+}  
+void loop()
+{
+  lcd.clear();
+  lcd.print(0, 0, "Water Level"); 
+   a=analogRead(A0);
+  Serial.println(a);
+ 
+  
+  lcd.print(7, 1, a);
+  Blynk.run();
+
+  delay(1500);
+
+}
---
+++ sketch_apr10b/sketch_apr10b.ino
@@ -0,0 +1,45 @@
+// 2 grids=910
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "c8717ecaf7404205bbdaff63c861e5cd";
+
+
+char ssid[] = "jahnavi";
+char pass[] = "12345678";
+
+WidgetLCD lcd(V1);
+int a;
+int x;
+
+void setup()
+{
+  
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  Blynk.begin(auth, ssid, pass);
+ 
+  lcd.clear(); 
+  lcd.print(0, 0, "Water Level in Centi Meter"); 
+
+}  
+void loop()
+{
+  
+  
+  lcd.clear();
+  lcd.print(0, 0, "Water Level in Centi Meter"); 
+   a=analogRead(A0);
+  
+
+  x=(1024-a)/63.33; 
+  Serial.println(x);
+  lcd.print(7, 1, x);
+  Blynk.run();
+
+  delay(1500);
+
+}
